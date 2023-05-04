5f68639a48e04a628a77ec703d3679196a10d1f3
Xiu <shiu2329@gmail.com>
2023-05-04 15:37:30 +0800

    up7

--- index.js
+++ index.js
@@ -20,22 +20,13 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: [
-      "https://mern-project-api-five.vercel.app",
-      "https://mern-project-client-ruby.vercel.app/",
-      "*",
-    ],
-  })
-);
-
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 app.options("*", cors());
